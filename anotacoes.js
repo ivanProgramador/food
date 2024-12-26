473f6b738d81d2611560affe2e0bc674e2d51c23
Ivan <ivan-guita@hotmail.com>
2024-12-26 00:24:58 -0300

    Tail wind configurado, conversor de tipos configurado, app rodando

--- anotacoes.js
+++ anotacoes.js
@@ -3,5 +3,14 @@
  como são recursos desnecessários nesse momnto então eu vou resetar o porjeto usando o comando abaixo
  
  mpm run reset-project
+ 
+ para a parte visual estou usando o native wind na documentação tem um passo a passo de como configurar 
+ tanto os tipos, quanto a biblioteca é importante resitar cadda etapa porque como estou lidando com android 
+ para que o aplicativo possa funcionar de forma nativas existem configurações que fazem essa conversão.
+ ATENÇÃO: A SDK ! eu estou usando acima de 50 !
+
+ 
+
+
 
 */

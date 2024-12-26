752fccf12eb40cfa219befcba2fa6956ed2bbd48
Ivan <ivan-guita@hotmail.com>
2024-12-26 00:45:14 -0300

    header pronto

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,9 +3,22 @@ import { Ionicons, Feather } from "@expo/vector-icons";
 
 export function Header() {
   return (
-    <View>
-      <Pressable>
+    <View className="bg-red-50 w-full items-center justify-between flex flex-row">
+      <Pressable className="w-10 h-10 bg-white rouded-full flex justify-center items-center">
         <Ionicons name="menu" size={20} color="#121212" />
+      </Pressable>
+
+      <View className="flex flex-col items-center justify-center">
+        <Text className="text-center text-sm text-slate-800">Localização</Text>
+
+        <View className="flex-row items-center justify-center gap-1">
+          <Feather name="map-pin" size={14} color="#FF0000" />
+          <Text className="text-lg font-bold">Fortaleza-ce</Text>
+        </View>
+      </View>
+
+      <Pressable className="w-10 h-10 bg-white rouded-full flex justify-center items-center">
+        <Feather name="bell" size={20} color="#121212" />
       </Pressable>
     </View>
   );

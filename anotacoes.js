7de77562349d84550a7320aa2796d2fd7c806c64
Ivan <ivan-guita@hotmail.com>
2024-12-25 23:24:31 -0300

    projeto iniciado

---
+++ anotacoes.js
@@ -0,0 +1,7 @@
+/*
+ quando um projeto usando react native é criado ele vem com alguns recuroisos de demonstração
+ como são recursos desnecessários nesse momnto então eu vou resetar o porjeto usando o comando abaixo
+ 
+ mpm run reset-project
+
+*/
